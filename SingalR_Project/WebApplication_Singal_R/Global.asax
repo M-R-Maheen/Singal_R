311c8f3b2a938312255fdcf0be3fa4c0c592792c
Mamunur Roshid Maheen <mamunurmaheen@gmail.com>
2023-02-15 17:55:39 +0600

    first commit

---
+++ SingalR_Project/WebApplication_Singal_R/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_Singal_R.MvcApplication" Language="C#" %>
---
+++ SingalR_Project/WebApplication_Singal_R/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(WebApplication_Singal_R.Startup))]
+
+namespace WebApplication_Singal_R
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
